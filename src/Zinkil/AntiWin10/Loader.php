4d7301333d042666539e586fa4f55b42131fbfaa
Zinkil-YT <mrzinkil2020@gmail.com>
2021-08-20 20:33:21 +0200

    Update 1.0.1

--- src/Zinkil/AntiWin10/Loader.php
+++ src/Zinkil/AntiWin10/Loader.php
@@ -1,14 +1,15 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Zinkil\AntiWin10;
 
+use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat as C;
-use pocketmine\Player;
-use pocketmine\Server;
 
 class Loader extends PluginBase implements Listener{
 
@@ -20,7 +21,7 @@ class Loader extends PluginBase implements Listener{
         $player = $event->getPlayer();
         $packet = $event->getPacket();
         if ($packet instanceof LoginPacket){
-            if ($packet->clientData["DeviceOS"] == 7){//7 is for Windows10
+            if ($packet->clientData["DeviceOS"] == 7){ //7 is for Windows10
                 $player->kick("§l§eYou are playing on Windows 10\n§r§cYou can't join this server with Win10", false);
             }
         }
